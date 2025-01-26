57f941758c44b1c44becae09c3e81a6027d98b7d
Martin Schimmel <onokje@gmail.com>
2025-01-26 15:41:23 +0100

    working basics

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
-import { readdirSync, readFileSync } from 'node:fs';
+import { readdirSync, readFileSync, writeFileSync, existsSync } from 'node:fs';
 import { Server } from 'socket.io';
 
 const app = express();
@@ -14,9 +14,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'src/index.html'));
 });
-app.get('/app.js', (req, res) => {
-    res.sendFile(join(__dirname, 'src/app.js'));
-});
+app.use('/assets', express.static('src/assets'))
 app.get('/api/notes', (req, res) => {
     const notes = readdirSync(join(__dirname, 'notes')).filter(note => note.endsWith('.md')).map(note => note.slice(0, -3));
 
@@ -29,12 +27,20 @@ app.get('/api/note/:note', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+    // socket.on('disconnect', () => {
+    //
+    // });
     socket.on('saveNote', (msg) => {
-        io.emit('noteSaved', msg);
+        console.log('Incoming saveNote: ' + msg.noteName);
+        if (existsSync(join(__dirname, 'notes/', `${msg.noteName}.md`))) {
+            writeFileSync(join(__dirname, 'notes/', `${msg.noteName}.md`), msg.noteContents);
+            io.emit('noteSaved', msg);
+        }
+    });
+    socket.on('newNote', (msg) => {
+        console.log('Incoming newNote: ' + msg.noteName);
+        writeFileSync(join(__dirname, 'notes/', `${msg.noteName}.md`), msg.noteContents);
+        io.emit('noteAdded', msg);
     });
 });
 
---
+++ src/assets/app.js
@@ -0,0 +1,91 @@
+const socket = io();
+const $notes = document.getElementById('notes');
+let selectedNote = null;
+let typingTimeout;
+let easyMDE;
+
+function setupEditor() {
+    let $editor = document.getElementById('main_editor');
+    if (!$editor) {
+        $editor = document.createElement('textarea');
+        $editor.setAttribute('id', 'main_editor');
+        document.getElementById('main_editor_container').appendChild($editor);
+
+        easyMDE = new EasyMDE({
+            element: $editor,
+            spellChecker: false
+        });
+        easyMDE.codemirror.on("change", () => {
+            clearTimeout(typingTimeout);
+            typingTimeout = setTimeout(() => {
+                socket.emit('saveNote', {noteName: selectedNote, noteContents: easyMDE.value()});
+            }, 400);
+        });
+    }
+}
+
+async function getNotes() {
+    try {
+        const response = await fetch("/api/notes");
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        $notes.innerHTML = '';
+        if (Array.isArray(data.notes)) {
+            data.notes.forEach((note) => {
+                const $noteEl = document.createElement('li');
+                $noteEl.innerHTML = note;
+                $noteEl.setAttribute('data-note', note);
+                $notes.appendChild($noteEl);
+            })
+        }
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+async function selectNote(noteName, targetElement) {
+    if (!noteName || noteName === '' || noteName === selectedNote) {
+        return;
+    }
+
+
+    try {
+        const response = await fetch(`/api/note/${noteName}`);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const noteDataMD = await response.text();
+
+
+        if (selectedNote) {
+            const currentSelectedNoteElement = document.querySelector('[data-note='+selectedNote+']');
+            if (currentSelectedNoteElement) {
+                currentSelectedNoteElement.classList.remove("active");
+            }
+        }
+
+        selectedNote = noteName;
+        targetElement.classList.add("active");
+        setupEditor();
+        easyMDE.value(noteDataMD);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+$notes.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.nodeName === 'LI') {
+        const targetNoteName = e.target.innerHTML;
+        selectNote(targetNoteName, e.target);
+    }
+
+});
+
+getNotes();
